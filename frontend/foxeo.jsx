50c2723efea4c922f7d46f2361c092a68003433c
roman <admin@MacBook-Pro-9.local>
2020-06-01 18:03:59 -0400

    Prepare for initial Heroku push

--- frontend/foxeo.jsx
+++ frontend/foxeo.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { siggnup, loggin, loggout } from './util/session_api_util'
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
+
+    //testing
+    window.siggnup = siggnup;
+    window.loggin = loggin;
+    window.loggout = loggout;
+    //testing
+
     ReactDOM.render(<h1>Welcome to Foxeo</h1>, root);
 });
