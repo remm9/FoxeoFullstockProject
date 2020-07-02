0fa7470e689d85c691447da3255110b091430d8c
roman <rg428@nyu.edu>
2020-07-02 17:27:57 -0400

    updated

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -6,5 +6,4 @@ import rootReducer from "../reducers/root_reducer";
 
 const configureStore = (preloadedState = {}) => (
     createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger)));
-
 export default configureStore;
