4d2c60f5db910770106a709692d10b4aa10f0814
roman <rg428@nyu.edu>
2020-07-04 12:42:49 -0400

    updated edit form

--- frontend/components/edit /edit.jsx
+++ frontend/components/edit /edit.jsx
@@ -4,9 +4,30 @@ class Edit extends React.Component {
 
     constructor(props) {
         super(props)
+        this.state = {
+            video_title: "",
+            video_description: "",
+        }
     }
     render() {
-        return null;
+        return (
+            <form action="">
+                <label>
+                    <input 
+                        type="text"
+                        value={this.state.video_title}
+                
+                    />
+                </label>
+                <label>
+                    <input 
+                        type="textarea"
+                        value={this.state.video_description}
+                    />  
+                </label>
+                <button>Edit</button>
+            </form>
+        );
     }
 }
 
