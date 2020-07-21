c13e6acf1a41526442931a3c0a3b1db2aa627298
roman <rg428@nyu.edu>
2020-07-21 12:04:38 -0400

    changed migration

--- db/migrate/20200715153433_add_presence_to_likes.rb
+++ db/migrate/20200715153433_add_presence_to_likes.rb
@@ -1,5 +1,10 @@
 class AddPresenceToLikes < ActiveRecord::Migration[5.2]
   def change
+    create_table :likes do |t|
+      t.integer :liker_id
+      t.integer :video_id
+      t.timestamps
+    end 
     change_column_null :likes, :liker_id, false
     change_column_null :likes, :video_id, false
     add_index :likes, :liker_id
