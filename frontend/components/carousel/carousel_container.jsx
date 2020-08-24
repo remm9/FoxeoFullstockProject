f96236059adaf0d762a56cc30d3a9693d72ac79f
roman <rg428@nyu.edu>
2020-08-24 09:58:32 -0400

    Created carousel container

---
+++ frontend/components/carousel/carousel_container.jsx
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import Carousel from './carousel';
+import { fetchVideos } from '../../actions/video_actions';
+import { fetchUsers } from '../../actions/user_actons';
+
+const mSTP = state => {
+    const currentUser = state.entities.users[state.session.id]
+    const videos = Object.values(state.entities.videos)
+    const users = Object.values(state.entities.users)
+    return({
+        currentUser,
+        videos,
+        users
+    })
+}
+
+const mDTP = dispatch => ({
+    fetchVideos: () => dispatch(fetchVideos()),
+    fetchUsers: () => dispatch(fetchUsers())
+})
+
+export default connect(mSTP, mDTP)(Carousel);
